744c4d11d8c959526b8a5d5069c25857cc4ee381
Mark Ashdown <maja1@mrao.cam.ac.uk>
2021-05-13 20:25:19 +0100

    SIM-864: Avoid using raw transaction methods

--- src/ska_sdp_proccontrol/workflow.py
+++ src/ska_sdp_proccontrol/workflow.py
@@ -11,8 +11,8 @@ def workflow_path(type, id, version):
 
 
 def create_workflow(txn, type, id, version, workflow):
-    txn.raw.create(workflow_path(type, id, version), workflow)
+    txn._create(workflow_path(type, id, version), workflow)
 
 
 def get_workflow(txn, type, id, version):
-    return txn.raw.get(workflow_path(type, id, version))
+    return txn._get(workflow_path(type, id, version))
